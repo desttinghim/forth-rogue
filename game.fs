625270d39cf3dca21479786285c28852b06b905f
Louis Pearson <desttinghimgame@gmail.com>
2017-02-22 01:30:01 -0600

    Screen now works properly. Always check for cells!

--- game.fs
+++ game.fs
@@ -1,33 +1,49 @@
 
+\ Constants
+
 20 constant width
 10 constant height
 width height * constant area
 
-variable screen
-area cells allot
+\ Variables
 
+variable screen area cells allot
+variable map area cells allot
+variable hero 1 cells allot
+
+\ Word definitions
+\ General
 : xy ( x y -- i )
   width * swap + ;
-
+: i-to-xy ( TODO: make this ) ;
+\ Screen
 : set-screen ( char i -- )
   cells screen + ! ;
 : print-screen ( -- )
-  page height 0 do  screen i width * + width cells type cr  loop ;
-
-variable map
-area cells allot
-
+  page height 0 do  width i * cells screen + width cells type cr  loop ;
+\ Map
 : set-map ( char i -- )  cells map + ! ;
 : get-map ( i -- char )  cells map + @ ;
 : fill-map ( char -- )
   area 0 do  dup i set-map  loop drop ;
-: generate-test-map ( -- )
-  area 0 do  i i set-map  loop ;
 : map-to-screen
   area 0 do  i get-map  i set-screen  loop ;
+\ Hero
+: place-hero ( i -- )
+  hero 1 cells + ! ;
+: locate-hero ( -- i )
+  hero 1 cells + @ ;
+: move-hero ( delta-x delta-y -- )
+  xy locate-hero + place-hero ;
+: hero-to-screen ( -- )
+  64 locate-hero set-screen ;
+
+: draw
+  map-to-screen
+  hero-to-screen
+  print-screen ;
+\ Runtime code
 
 46 fill-map
-map-to-screen
-print-screen
-
-variable hero
+9 4 xy place-hero
+draw
