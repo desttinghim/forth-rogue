68a1bd084d6d24cc9ae5f92348036b7e23dd96a6
Louis Pearson <desttinghimgame@gmail.com>
2017-02-24 13:19:20 -0600

    Added boundaries to the map.

--- game.fs
+++ game.fs
@@ -4,6 +4,7 @@
 20 constant width
 10 constant height
 width height * constant area
+46 constant empty
 
 \ Variables
 
@@ -13,47 +14,41 @@ variable hero 1 cells allot
 
 \ Word definitions
 \ General
-: xy ( x y -- i )
-  width * swap + ;
+: xy ( x y -- i )  width * swap + ;
 : i-to-xy ( TODO: make this ) ;
+
 \ Screen
-: set-screen ( char i -- )
-  cells screen + ! ;
+: set-screen ( char i -- )  cells screen + ! ;
 : print-screen ( -- )
   page height 0 do  width i * cells screen + width cells type cr  loop ;
+
 \ Map
 : set-map ( char i -- )  cells map + ! ;
 : get-map ( i -- char )  cells map + @ ;
-: fill-map ( char -- )
-  area 0 do  dup i set-map  loop drop ;
-: map-to-screen
-  area 0 do  i get-map  i set-screen  loop ;
+: fill-map ( char -- )  area 0 do  dup i set-map  loop drop ;
+: map-to-screen ( -- )  area 0 do  i get-map  i set-screen  loop ;
+
 \ Hero
-: place-hero ( i -- )
-  hero 1 cells + ! ;
-: locate-hero ( -- i )
-  hero 1 cells + @ ;
+: place-hero ( i -- )   hero 1 cells + ! ;
+: locate-hero ( -- i )  hero 1 cells + @ ;
 : move-hero ( delta-x delta-y -- )
-  xy locate-hero + place-hero ;
-: hero-to-screen ( -- )
-  64 locate-hero set-screen ;
+  xy locate-hero +
+  dup 0 > if dup area < if
+  dup get-map empty = if place-hero else drop then then then ;
+: hero-to-screen ( -- )  64 locate-hero set-screen ;
 
-: draw
-  map-to-screen
-  hero-to-screen
-  print-screen ;
-
+\ Control
 : input
   key dup 119 = if  0 -1 move-hero  else
-      dup 97  = if  -1 0 move-hero  else
-      dup 115 = if  0 1 move-hero  else
-      dup 100 = if  1 0 move-hero  else
+      dup 97  = if -1  0 move-hero  else
+      dup 115 = if  0  1 move-hero  else
+      dup 100 = if  1  0 move-hero  else
   then then then then ;
-
-: update  begin input draw 0 until ;
+: draw  map-to-screen  hero-to-screen  print-screen ;
+: gameloop  begin input draw 0 until ;
 \ Runtime code
 
-46 fill-map
+empty fill-map
 9 4 xy place-hero
 draw
-update
+gameloop
